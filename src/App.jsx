34f86c42f1ef684c4286d40d6528932bc2e1a607
breakpack <198799879a@gmail.com>
2025-11-04 12:33:44 +0900

    refactorFeat: 디렉토리 구조 변경 #8

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import AboutUs from "./pages/AboutUs";
-import ProjectList from "./pages/Project/ProjectList";
-import ProjectCreate from "./pages/Project/ProjectCreate";
+import AboutUs from "./pages/landingPage/AboutUs";
+import ProjectList from "./pages/projects/projectList/ProjectList";
+import ProjectCreate from "./pages/projects/projectCreate/ProjectCreate";
 import GlobalStyle from "./styled/Global";
 
 function App() {
-    return (
-        <>
-            <GlobalStyle />
-            <Router>
-                <Routes>
-                    <Route path="/aboutus" element={<AboutUs />} />
-                    <Route path="/projects" element={<ProjectList />} />
-                    <Route path="/projects/new" element={<ProjectCreate />} />
-                </Routes>
-            </Router>
-        </>
-    );
+  return (
+    <>
+      <GlobalStyle />
+      <Router>
+        <Routes>
+          <Route path="/aboutus" element={<AboutUs />} />
+          <Route path="/projects" element={<ProjectList />} />
+          <Route path="/projects/new" element={<ProjectCreate />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
